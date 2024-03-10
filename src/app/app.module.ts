d9bbfc7adccd0cd6dda30e40ec9e6c384ffa283d
preeti12309 <preetikumart954@gmail.com>
2024-03-11 02:33:43 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ChangeBgDirective } from './change--bg.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    ChangeBgDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
